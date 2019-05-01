cb0eb8e9d05caccee3503934cbc720f0c366ca77
Esraa <esraa.nasserfciscu@gmail.com>
2019-05-01 17:44:45 +0200

    Add Queries Playground File

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,19 +1,30 @@
 const {mongoose}=require('./../server/DB/mongoose');
 const {Todo}=require('./../server/models/Todo');
-const {objectID}=require('mongodb');
+const {ObjectID}=require('mongodb');
+const {User}=require('./../server/models/User')
 
-var id = '5cc788b2a39e4a8c82635f78';
-Todo.find({_id:id}).then((todos)=>{
-    console.log('todos: ',todos)
-},(err)=>{
-    console.log('unable to find obj')
-})
-Todo.findOne({_id:id}).then((todos)=>{
-console.log('todos: ',todos)
-})
+var User_Id='5cc6e8a4d4e3831287688579';
 
-Todo.findById(id).then((todo)=>{
-    console.log(todo);
+User.find({_id:User_Id}).then((res)=>{
+    if(ObjectID.isValid(User_Id))
+        return console.log('Valid ID ',res);
 },(err)=>{
-    console.log(err);
+   return console.log('Unable to Find Obj')
+}).catch((e)=>{
+    console.log(e)
 })
+// var id = '5cc788b2a39e4a8c82635f78';
+// Todo.find({_id:id}).then((todos)=>{
+//     console.log('todos: ',todos)
+// },(err)=>{
+//     console.log('unable to find obj')
+// })
+// Todo.findOne({_id:id}).then((todos)=>{
+// console.log('todos: ',todos)
+// })
+
+// Todo.findById(id).then((todo)=>{
+//     console.log(todo);
+// },(err)=>{
+//     console.log(err);
+// })
